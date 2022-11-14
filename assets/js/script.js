747b3c21431cfea431ba3b3157cdd2058c22863d
Alexander Barlow <alex.barlow@rocketmail.com>
2022-11-14 15:14:45 -0500

    Added js variables

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+var window = window.document;
+var highscores = document.getElementById('highscores');
+var time = document.getElementById('time');
+var start = document.getElementById('start');
+var allDone = document.getElementById('all-done');
+var highscoresList = document.getElementById('highscores-list');
+var questions = document.getElementsByClassName('question');
+var button = document.querySelectorAll('button')
+
+
+
+
+
+
+
+
+
+window.addEventListener('click', Start)
+window.addEventListener('click', Start)
+window.addEventListener('click', Start)
+window.addEventListener('click', Start)
+window.addEventListener('click', Start)
