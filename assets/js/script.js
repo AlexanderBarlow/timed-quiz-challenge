d325e2720607bc674f58946b3e98acfcd007e9d2
Alexander Barlow <alex.barlow@rocketmail.com>
2022-11-14 16:15:10 -0500

    Reworked Html and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,24 @@
-var window = window.document;
+
 var highscores = document.getElementById('highscores');
 var time = document.getElementById('time');
 var start = document.getElementById('start');
 var allDone = document.getElementById('all-done');
 var highscoresList = document.getElementById('highscores-list');
-var questions = document.getElementsByClassName('question');
-var button = document.querySelectorAll('button')
+var question = document.getElementById('question');
+var choice1 = document.getElementsByClassName('option1');
+var choice2 = document.getElementsByClassName('option2');
+var choice3 = document.getElementsByClassName('option3');
+var choice4 = document.getElementsByClassName('option4');
+var startButton = document.getElementById('start-button');
+
+var questionForms = [question1, question2, question3, question4, question5];
+var userInput;
+
+
+function questionEval {
+    this.score = 0;
+}
 
 
 
 
-
-
-
-
-
-window.addEventListener('click', Start)
-window.addEventListener('click', Start)
-window.addEventListener('click', Start)
-window.addEventListener('click', Start)
-window.addEventListener('click', Start)
